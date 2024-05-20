77b6e65cb13d4877882ec5bdf7e5af75e402a596
Mubashir <app@Mubashir-MBP.lan>
2024-05-20 20:27:57 +0500

    worked on user schema

---
+++ src/db/users.ts
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+
+const UserSchema = new mongoose.Schema({
+    username: { type: String, require: true },
+    email: { type: String, require: true, },
+    authentication: {
+        password: { type: String, required: true, select: false },
+        salt: { type: String, select: false },
+        sessionToken: { type: String, select: false }
+    }
+})
+
+export const UserModel = mongoose.model('User', UserSchema)
