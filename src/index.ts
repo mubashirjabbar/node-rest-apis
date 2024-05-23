a32c816aa3a2f1fafcf0be7737985e326e3f45f9
Mubashir <app@Mubashir-Macbook-Pro.local>
2024-05-23 15:36:03 +0500

    worked on node apis with add user into mongo bd table

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import cookieParser from 'cookie-parser'
 import compression from 'compression'
 import cors from 'cors'
 import mongoose, { Mongoose } from 'mongoose'
-
+import router from './router'
 
 const app = express();
 
@@ -19,16 +19,13 @@ app.use(bodyParser.json());
 
 const server = http.createServer(app);
 
-server.listen(8080, () => {
-    console.log("server is up");
-
-})
+server.listen(8080, () => { console.log("server is up") })
 
 const MONGOS_URL = "mongodb+srv://mubashirjabbar97:Mubashir@node-testing-cluster.sux8rcd.mongodb.net/"
 
 mongoose.Promise = Promise
-mongoose.connect(MONGOS_URL).then(()=>{
-    console.log("connected");
-    
-})
+mongoose.connect(MONGOS_URL).then(() => { console.log("connected") })
 mongoose.connection.on('error', (error: Error) => console.log(error))
+
+app.use('/', router())
+
