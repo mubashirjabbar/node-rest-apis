ffca6a8d1d364c49c9ef2eb740da820317e86bab
Mubashir <app@apps-MBP.lan>
2024-08-19 18:23:27 +0500

    feat: implement delete and update user

--- src/router/user.ts
+++ src/router/user.ts
@@ -1,10 +1,12 @@
 import express from "express"
 
-import { deleteUser, getAllUser } from "../controllers/user"
-import { isAuthenticated } from "../middleware"
+import { deleteUser, getAllUser, updateUser } from "../controllers/user"
+import { isAuthenticated, isOwner } from "../middleware"
 
 export default (router: express.Router) => {
     router.get('/users', isAuthenticated, getAllUser)
-    router.delete('/users/:id', deleteUser)
+    router.delete('/users/:id', isAuthenticated, isOwner, deleteUser)
+    router.patch('/users/:id', isAuthenticated, isOwner, updateUser)
+
     return router;
 }
